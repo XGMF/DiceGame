1537629b89e03d9d9c70611c07c3d781f6e42717
Lee <ltinsdale.nz@gmail.com>
2019-09-22 14:51:15 +1200

    Added game more game logic. Refactored some code. Computer yet to be implemented.

--- src/main/java/nz/ac/eit/DiceGame/Game.java
+++ src/main/java/nz/ac/eit/DiceGame/Game.java
@@ -2,10 +2,6 @@ package nz.ac.eit.DiceGame;
 
 import java.util.Scanner;
 
-
-
-
-
 class Game {
 
 	Player player1;
@@ -17,8 +13,9 @@ class Game {
     
     InputCollector input = null;
     SupportedInput  supportedInput = null;
-    
-    
+
+    private boolean switchPlayer =false;
+
     /**
      *  Game object constructor
      */
@@ -33,116 +30,90 @@ class Game {
     
     
     public void play() {
+
         Player currentPlayer = player1; // start with this player
-        int currentDice1;
-        int currentDice2;
 
-            // play dice
-            currentDice1 = Dice.roll();
-            currentDice2 = Dice.roll();
-            
-            System.out.print(currentDice1 +" "+currentDice2+"\n"+player1.sum(currentDice1, currentDice2));
-            
-            // update score of current player
-            //currentPlayer.updateScore(sum(currentDice1,currentDice2));
-            
-            // switch players
-            //if(currentPlayer == player1) {
-            //    currentPlayer = player2;
-            
-       	 
-       	 System.out.println("\n"+ "Please Roll or Hold (R/H)" );
-       	 String inputAsString=scan.nextLine().toUpperCase();
-       	 
-       	 try {
-       		 supportedInput=collector.collectString(inputAsString);
-            }
-            catch (StringNotValidInputException lnse){
-                System.out.println(lnse.getMessage());
-                System.exit(1);
-            }
-       	 
-       	 RollOrHold(inputAsString);
-            
-            
+            while(isUnderGameLimit(currentPlayer.getRunningTotal())) {////////////////////////////////////////////////////////////////////
 
+                // play dice
+                int currentDice1 = Dice.roll();
+                int currentDice2 = Dice.roll();
+                int aSum =currentPlayer.sum(currentDice1,currentDice2);
+                int aRunningTotal= currentPlayer.runningTotal(aSum);
+
+                System.out.println(currentPlayer);
+                System.out.print(currentDice1 + " " + currentDice2 + "\n" + aSum+"\n"+"Running Total :"+aRunningTotal);
+
+                System.out.println("\n" + "Please Roll or Hold (R/H)");
+                String inputAsString = scan.nextLine().toUpperCase();
+
+                try {
+                    supportedInput = collector.collectString(inputAsString);
+                } catch (StringNotValidInputException lnse) {
+                    System.out.println(lnse.getMessage());
+                    System.exit(1);
+                }
+
+                //asks user if it wants to Roll or Hold, will switch to computer on hold, or vice versa
+                RollOrHold(inputAsString);
+                //checks whether the total is over the ending condition
+                isUnderGameLimit(aRunningTotal);
+
+                /**
+                 * Player check
+                 * if the player has chosen to hold, the current player us switched.
+                 */
+                if(currentPlayer == player1&&switchPlayer==true) {
+                    currentPlayer = player2;
+                } else {
+                    currentPlayer = player1;
+                }
+            }///////////////////////////////////////////////////////////////////////
     }
-    
+
+    /**
+     *Checks if the player holds or rolls.
+     * only checks one condition at the moment
+     */
 	public String RollOrHold(String roll) {
 		
 		//System.out.print(roll);
 		
 		if(roll.contentEquals("H")) {
-			System.out.print("The player has chosen to hold.\n "+"It is now the computers turn");
-			
+			System.out.print("The player has chosen to hold.\n "+"It is now the computers turn"+"\n");
+            switchPlayer=true;
+
+            ////TODO computer
+
+			/**
+             * If the player holds, it is the computers turn.
+             * Vice versa, if its the computers turn and holds
+             * it is the players turn.
+             */
 		}else {
 			System.out.print("The player has rolled the dice\n");
-			//int dice1= myDice.RollDieOne();
-			//int dice2= myDice.RollDieTwo();
-			
-		
+            switchPlayer=false;
+			/**
+             * If the player rolls the dice nothing happens here.
+             * Returns to the beginning of the loop
+			 */
 		}
-		
-		
-		
 		return roll;
-		
 	}
-	
+
+    /**
+     * Responsible for checking limit conditions.
+     */
+    public static boolean isUnderGameLimit(int value) {
+
+        if (value > 21) {
+            System.out.println("Over 21");
+            return false;
+        } else {
+            System.out.println("Less Than21");
+            return true;
+        }
+    }
     
     
 }
-    
-
-
-    /**
-     *  Display winner based on score value
-     */
-    /**
-    private Player winner() {
-        if(player1.score > player2.score) {
-            return player1;
-        } else if(player2.score > player1.score) {
-            return player2;
-        } else {
-            return null;
-        }
-    }
-    
-    
-     *  Play the game with following sequential steps:
-     *  1. Roll dice
-     *  2. Update score of current player based on dice value from above step.
-     *  3. Switch current player.
-     *  4. Repeat steps from 1 untill playCounter is zero (repeat that many times).
-     *  5. Outside above loop, check which player has higher score. Display winner, or tie.
-    
-    public void play() {
-        Player currentPlayer = player1; // start with this player
-        int currentDice;
-        int playCounter = 10;  // play till this count
-        
-        while(playCounter-- > 0) {
-            // play dice
-            currentDice = Dice.roll();
-            
-            // update score of current player
-            currentPlayer.updateScore(currentDice);
-            
-            // switch players
-            if(currentPlayer == player1) {
-                currentPlayer = player2;
-            } else {
-                currentPlayer = player1;
-            }
-        }
-        
-        // display winner, or game tie
-        if(winner() != null) {
-            System.out.println(winner() + " WINS !");
-        } else {
-            System.out.println("Game tie !");
-        }
-    }
-     */
-    
--- src/main/java/nz/ac/eit/DiceGame/Player.java
+++ src/main/java/nz/ac/eit/DiceGame/Player.java
@@ -2,37 +2,35 @@ package nz.ac.eit.DiceGame;
 
 public class Player {
 
-	 int id;
-	    String name;
-	    int score;
-	    
+	 	private int id;
+	    private String name;
+	    private int runningTotal;
+
 	    public Player(int id, String name) {
 	        this.id = id;
 	        this.name = name;
-	        this.score = 0;
+	        this.runningTotal=0;
 	    }
 	    
 	    /**
 	     *  Update player score
 	     */
-	    public void updateScore(int sumOfCurrentDice) {
-	        score += sumOfCurrentDice;
-	        System.out.format("\n%s \tscore = %d\n", name, score);
-	    }
-	    
+
 	    public int sum(int dice1, int dice2) {
 	    	int sum=(dice1+dice2);
 	    	return sum;
 	    }
-	    
-	    /**
-	     * 
-	     */
-	    
-	    
-	    @Override
-	    public String toString() {
-	        return name + " [" + score + "]";
-	    }
-	
+
+	    public int runningTotal(int aSum){
+	    	 return runningTotal+=aSum;
+		}
+
+
+	public int getRunningTotal() {
+		return runningTotal;
+	}
+
+	public void setRunningTotal(int runningTotal) {
+		this.runningTotal = runningTotal;
+	}
 }
